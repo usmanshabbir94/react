c979449f72896199010dec68026e4f3ec98e10ca
usman <usman.shabbir@kwanso.com>
2017-06-20 18:13:04 +0500

    server change

--- server/server.js
+++ server/server.js
@@ -23,3 +23,6 @@ app.listen(port, function(error) {
   if (error) throw error;
   console.log("Express server listening on port", port);
 });
+server.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
